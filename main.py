3a371feb0e4ccabebdca9bc4d7ac1d9d07f5a320
HannyShin <112891652+HannyShin@users.noreply.github.com>
2022-10-31 18:28:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from functions import (bed1_handler, off_handler)
 
 
 while True:
-    
+    # one wire to GPIO & the other to 3.3OUT
+    # bed1 uses blue & blue/white wire
     bed1_handler()
-    utime.sleep_ms(400)
+    # off uses brown & brown/white wire
     off_handler()
-    utime.sleep_ms(400)
