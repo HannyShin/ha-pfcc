032015f6e5b0140f93481f9d7c77a3a09a02f951
HannyShin <112891652+HannyShin@users.noreply.github.com>
2022-10-31 18:18:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from functions import (bed1_down_handler, off_btn_handler)
 
 while True:
     
-    bed1_down_handler()
+    bed1_handler()
     utime.sleep_ms(400)
     off_btn_handler()
     utime.sleep_ms(400)
